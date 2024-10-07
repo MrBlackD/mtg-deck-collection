3d9b6ed278584fe6c41e6d1deb63b4f562b48d32
MrBlackD <blackpirates@mail.ru>
2024-10-07 21:55:22 +0300

    makeCard with cycle

--- script.js
+++ script.js
@@ -19,9 +19,38 @@ const makeCard = (cardName, description, imageLink) => {
     content.appendChild(cardDiv)
 }
 
+const cardsData = [
+    {
+        "name": "Gonti, Canny Acquisitor",
+        "description": "Theft",
+        "imageLink": "https://cards.scryfall.io/normal/front/0/3/03a7e79f-625a-49ac-9cb1-e1fe5f51f5a0.jpg?1714110365"
+    },
+    {
+        "name": "Katilda, Dawnhart Prime",
+        "description": "+1/+1 counters / Humans",
+        "imageLink": "https://cards.scryfall.io/normal/front/6/7/673cd775-d417-4652-a91d-825ad5c89e8a.jpg?1636684486"
+    },
+    {
+        "name": "Ulalek, Fused Atrocity",
+        "description": "Copy Eldrazi Spells",
+        "imageLink": "https://cards.scryfall.io/normal/front/f/d/fdad1b0e-d3cc-4d76-ae7e-fee12558cf2c.jpg?1717644357"
+    },
+    {
+        "name": "Satya, Aetherflux Genius",
+        "description": "Enegry",
+        "imageLink": "https://cards.scryfall.io/normal/front/3/b/3b964bbe-54cc-425c-9cc6-c877f82af7ba.jpg?1717644351"
+    },
+    {
+        "name": "Zedruu the Greathearted",
+        "description": "Stax",
+        "imageLink": "https://cards.scryfall.io/normal/front/e/9/e9ea2a6b-5aaf-4178-b945-f409fe83c41a.jpg?1562424565"
+    }
+]
+
 window.onload = function() {
-    makeCard("Gonti, Canny Acquisitor", "Theft", "https://cards.scryfall.io/normal/front/0/3/03a7e79f-625a-49ac-9cb1-e1fe5f51f5a0.jpg?1714110365")
-    makeCard("Katilda, Dawnhart Prime", "+1/+1 counters / Humans", "https://cards.scryfall.io/normal/front/6/7/673cd775-d417-4652-a91d-825ad5c89e8a.jpg?1636684486")
+    for (const card of cardsData) {
+        makeCard(card.name, card.description, card.imageLink)
+    }
 }
 
 {/* <div class="card">
