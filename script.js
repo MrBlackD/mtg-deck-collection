b714249ce769d3bb8f7d3154105d91e174ca3769
MrBlackD <blackpirates@mail.ru>
2024-10-07 21:48:38 +0300

    plain make cards with js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-window.onload = function() {
-    const content = document.querySelector('.content')
-    content.textContent = 'Hello World'
-    document.getElementById('myButton').onclick = function() {
-        document.getElementById('modal').classList.toggle('hidden')
-    }
+const makeCard = (cardName, description, imageLink) => {
+    const content = document.getElementById("content")
+
+    const cardDiv = document.createElement('div')
+    cardDiv.className = "card"
+
+    const h2 = document.createElement('h2')
+    h2.textContent = cardName
+    cardDiv.appendChild(h2)
+    
+    const img = document.createElement('img')
+    img.src = imageLink
+    cardDiv.appendChild(img)
+
+    const p = document.createElement('p')
+    p.textContent = description
+    cardDiv.appendChild(p)
+
+    content.appendChild(cardDiv)
 }
+
+window.onload = function() {
+    makeCard("Gonti, Canny Acquisitor", "Theft", "https://cards.scryfall.io/normal/front/0/3/03a7e79f-625a-49ac-9cb1-e1fe5f51f5a0.jpg?1714110365")
+    makeCard("Katilda, Dawnhart Prime", "+1/+1 counters / Humans", "https://cards.scryfall.io/normal/front/6/7/673cd775-d417-4652-a91d-825ad5c89e8a.jpg?1636684486")
+}
+
+{/* <div class="card">
+            <h2>Gonti, Canny Acquisitor</h2>
+            <img src="https://cards.scryfall.io/normal/front/0/3/03a7e79f-625a-49ac-9cb1-e1fe5f51f5a0.jpg?1714110365">
+            <p>Theft</p>
+        </div> */}
